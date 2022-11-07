b290cb9a4f4ccbdac79af95c3dd038c16140c9a1
Ilya <ilyayurch@gmail.com>
2022-11-07 03:43:36 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,30 +1,39 @@
 package ru.job4j.chess.firuges.black;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 public class BishopBlackTest {
     @Test
     public void whenPositionTest() {
         Cell cell = Cell.F8;
-        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
-        assertThat(bishopBlack.position(), is(cell));
+        BishopBlack bishopBlack = new BishopBlack(cell);
+        Cell currentPosition = bishopBlack.position();
+        assertThat(currentPosition).isEqualTo(cell);
     }
 
     @Test
     public void whenCopyTest() {
-        Cell cellend = Cell.D6;
-        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
-        assertThat(cellend, is(bishopBlack.copy(Cell.D6).position()));
+        Cell firstPosition = Cell.C8;
+        Cell secondPosition = Cell.G8;
+        BishopBlack firstBishop = new BishopBlack(firstPosition);
+        Figure secondBishop = firstBishop.copy(secondPosition);
+        Cell currentPosition = secondBishop.position();
+        assertThat(currentPosition).isEqualTo(secondPosition);
     }
 
     @Test
     public void whenWayTest() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        Cell[] bishopDiagonal = bishopBlack.way(Cell.G5);
-        Cell[] bishopEnd = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
-        assertThat(bishopEnd, is(bishopDiagonal));
+        Cell startPosition = Cell.C1;
+        Cell endPosition = Cell.G5;
+        Cell[] expectedCells = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        BishopBlack firstBishop = new BishopBlack(startPosition);
+        firstBishop.way(endPosition);
+        assertThat(firstBishop.way(endPosition)).isEqualTo(expectedCells);
     }
 }
